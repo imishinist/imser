64ad5c53e287a3b14cc6cf6d5e3dd1f9cfa16176
Taisuke Miyazaki <imishinist@gmail.com>
2022-02-13 18:41:07 +0900

    add DocAndScore test

--- src/doc.rs
+++ src/doc.rs
@@ -34,7 +34,7 @@ impl DocAndScore {
         if sub.abs() <= f32::EPSILON {
             return Ordering::Equal;
         }
-        // self > score
+        // self > other
         if sub > 0f32 {
             Ordering::Greater
         } else {
@@ -47,15 +47,69 @@ impl Eq for DocAndScore {}
 
 impl PartialOrd<Self> for DocAndScore {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        self.score
-            .partial_cmp(&other.score)
-            .map(|f| f.then_with(|| self.doc_id.cmp(&other.doc_id)))
+        Some(self.cmp(other))
     }
 }
 
 impl Ord for DocAndScore {
     fn cmp(&self, other: &Self) -> Ordering {
         self.score_cmp(other)
-            .then_with(|| self.doc_id.cmp(&other.doc_id).reverse())
+            .reverse()
+            .then_with(|| self.doc_id.cmp(&other.doc_id))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::DocAndScore;
+    use std::cmp::Ordering;
+
+    #[test]
+    fn doc_and_score_compare_test() {
+        let doc1 = DocAndScore::new_with_score(1, 1.0);
+        let doc2 = DocAndScore::new_with_score(2, 1.0);
+        let doc3 = DocAndScore::new_with_score(3, 0.0);
+        let doc4 = DocAndScore::new_with_score(4, 1.0);
+
+        assert_eq!(doc1.cmp(&doc2), Ordering::Less);
+        assert_eq!(doc2.cmp(&doc1), Ordering::Greater);
+        assert_eq!(doc1.cmp(&doc1), Ordering::Equal);
+        assert_eq!(doc1.cmp(&doc3), Ordering::Less);
+        assert_eq!(doc3.cmp(&doc1), Ordering::Greater);
+        assert_eq!(doc3.cmp(&doc4), Ordering::Greater);
+        assert_eq!(doc4.cmp(&doc3), Ordering::Less);
+
+        assert_eq!(doc1.partial_cmp(&doc2), Some(Ordering::Less));
+        assert_eq!(doc2.partial_cmp(&doc1), Some(Ordering::Greater));
+        assert_eq!(doc1.partial_cmp(&doc1), Some(Ordering::Equal));
+        assert_eq!(doc1.partial_cmp(&doc3), Some(Ordering::Less));
+        assert_eq!(doc3.partial_cmp(&doc1), Some(Ordering::Greater));
+        assert_eq!(doc3.partial_cmp(&doc4), Some(Ordering::Greater));
+        assert_eq!(doc4.partial_cmp(&doc3), Some(Ordering::Less));
+    }
+
+    #[test]
+    fn doc_and_score_sort_test() {
+        let mut docs = vec![
+            DocAndScore::new_with_score(1, 0.0),
+            DocAndScore::new_with_score(0, 0.0),
+            DocAndScore::new_with_score(2, 0.0),
+        ];
+        docs.sort();
+        assert_eq!(
+            docs.iter().map(|d| d.doc_id).collect::<Vec<_>>(),
+            vec![0, 1, 2]
+        );
+
+        let mut docs = vec![
+            DocAndScore::new_with_score(1, 0.0),
+            DocAndScore::new_with_score(0, 0.5),
+            DocAndScore::new_with_score(2, 0.5),
+        ];
+        docs.sort();
+        assert_eq!(
+            docs.iter().map(|d| d.doc_id).collect::<Vec<_>>(),
+            vec![0, 2, 1]
+        );
     }
 }
